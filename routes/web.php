98277cc57d3dcc697fe437945f2368a5097083fb
cmo7 <marcelinocb@gmail.com>
2021-07-29 21:14:21 +0100

    Modelo de datos actualizado

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('portada');
+    return view('portada', [
+        "recetas" => Receta::all();
+    ]);
 });
 
 Route::get('/dashboard', function () {
